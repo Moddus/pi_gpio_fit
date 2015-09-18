450cc3e549b99425971d18bbf29ab16966b49dc6
root <root@bananapi.(none)>
2015-09-18 14:16:26 +0000

    Permissions changed. LED order correct

--- src/main.c
+++ src/main.c
@@ -8,6 +8,7 @@
 #define FALSE 0
 #define ON TRUE
 #define OFF FALSE
+#define DELAY 100
 
 void wiringpi_init() {
 	if (wiringPiSetup() == -1)
@@ -28,30 +29,38 @@ void wiringpi_delay(long t) {
 	delay(t);
 }
 
-int main() {
+int main(int argc, char* argv[]) {
 	wiringpi_init();
-	MPI_Init(NULL, NULL);
+	MPI_Init(&argc, &argv);
 
 	int world_rank;
 	MPI_Comm_rank(MPI_COMM_WORLD, &world_rank);
 	int world_size;
 	MPI_Comm_size(MPI_COMM_WORLD, &world_size);
+    printf("%d/%d running\n", world_rank, world_size);
 
 	int token;
-	if (world_rank != 0) {
-		MPI_Recv(&token, 1, MPI_INT, world_rank - 1, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-		wiringpi_led_on();		
-		wiringpi_delay(500);
-		wiringpi_led_off();
-	} else {
-		token = -1;
-	}
+    if(world_rank == 0){
+	    MPI_Send(&token, 1, MPI_INT, (world_rank + 1) % world_size, 0, MPI_COMM_WORLD);
+    }
+
+    while(TRUE){
+        if (world_rank != 0) {
+            MPI_Recv(&token, 1, MPI_INT, world_rank - 1, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+            wiringpi_led_on();		
+            wiringpi_delay(DELAY);
+            wiringpi_led_off();
+	        MPI_Send(&token, 1, MPI_INT, (world_rank + 1) % world_size, 0, MPI_COMM_WORLD);
+        } else {
+            MPI_Recv(&token, 1, MPI_INT, world_size-1 , 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+            wiringpi_led_on();		
+            wiringpi_delay(DELAY);
+            wiringpi_led_off();
+	        MPI_Send(&token, 1, MPI_INT, (world_rank + 1) % world_size, 0, MPI_COMM_WORLD);
+        }
+    }
 	
-	MPI_Send(&token, 1, MPI_INT, (world_rank + 1) % world_size, 0, MPI_COMM_WORLD);
-	
-	if (world_rank == 0) {
-		MPI_Recv(&token, 1, MPI_INT, world_size - 1, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-	}
 
 	MPI_Finalize();
 }
+
